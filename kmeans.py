235db57d1dd70bffa166c210e9b6b8a714269aea
emankcin <joknaut@gmx.net>
2016-07-27 00:18:23 +0200

    Improve kmeans implementation
    Remove global variables in favour of function parameters.
    Remove inner k variable to allow setting k only in the function call.
    Introduce helper function kmeans_h to handle initialization before recursion.

--- kmeans.py
+++ kmeans.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-k = 5
-
 csv_path = "2d-sample.csv"
 dim = 2
 delimiter = ","
@@ -15,40 +13,54 @@ f_size = (25, 10)
 
 dataset = pd.read_csv(csv_path, "r", delimiter=delimiter, engine=engine, header=None)
 
-mu0 = np.random.rand(k, dim)
-for i in range(k):
-    mu0[i] = mu0[i] * ((i+1) / float(k)) * dataset.max() - dataset.max()/2
-mu = pd.DataFrame(mu0)
-clusters = []
-for i in range(k):
-    clusters.append([])
+minX = min(dataset[0])
+minY = min(dataset[1])
+maxX = max(dataset[0])
+maxY = max(dataset[1])
+xDist = np.absolute(maxX - minX)
+yDist = np.absolute(maxY - minY)
 
-def kmeans(data, k):
+
+def kmeans(data, k=4):
+    centroids = []  
+    for i in range(k):
+        centroids.append([minX + np.random.rand() * xDist, minY + np.random.rand() * yDist])
+    mus = pd.DataFrame(centroids)
+    clusters = []
+    for i in range(k):
+        clusters.append([])
+    kmeans_h(data, k, centroids, clusters, mus)
+
+def kmeans_h(data, k, centroids, clusters, mus):
     ax = data.plot(kind="scatter", x=0, y=1, figsize=f_size)
-    plotMeans(ax, mu, mu0, k)
+    plotMeans(ax, mus, centroids, k)
     plt.show()
     for i in range(k):
         clusters[i] = []
     for i in range(len(dataset)):
-        idx = getNearestMeanIndex([data[0][i], data[0][i]], mu0)
+        idx = getNearestMeanIndex([data[0][i], data[1][i]], centroids)
         clusters[idx].append([data[0][i], data[1][i]])
-    hasChanged = updateMeans(clusters)
+    (hasChanged, centroids, mus) = updateMeans(clusters, centroids)
     if True == hasChanged:
-        kmeans(data, k)
-
-def updateMeans(clusters):
-    global mu0
-    global mu
+        kmeans_h(data, k, centroids, clusters, mus)        
+        for i in range(k):
+            for j in range(k):
+                if (centroids[i][0] - centroids[j][0])**2 + (centroids[i][1] - centroids[j][1])**2 < 5:
+                    centroids[j] = [minX + np.random.rand() * xDist, minY + np.random.rand() * yDist]
+    
+def updateMeans(clusters, centroids):
     flag = False
     for i in range(len(clusters)):
         if clusters[i] == []:
-            continue
+            centroids[i] = [minX + np.random.rand() * xDist, minY + np.random.rand() * yDist] 
+            continue       
         if flag == False:
-            if np.absolute(mu0[i][0] - np.mean(clusters[i], axis=0)[0]) > 0.01:
+            if np.absolute(centroids[i][0] - np.mean(clusters[i], axis=0)[0]) > 0.01:
                 flag = True
-        mu0[i] = np.mean(clusters[i], axis=0)
-    mu = pd.DataFrame(mu0)
-    return flag
+        centroids[i][0] = np.mean(clusters[i], axis=0)[0]
+        centroids[i][1] = np.mean(clusters[i], axis=0)[1]
+    mus = pd.DataFrame(centroids)
+    return (flag, centroids, mus)
 
 def getNearestMeanIndex(point, means):
     dists = []
@@ -60,12 +72,11 @@ def getNearestMeanIndex(point, means):
             minidx = i
     return minidx
 
-def plotMeans(ax, mu, mu0, k):
-    mu.plot(ax = ax, kind="scatter", x=0, y=1, figsize=f_size, color="red")
+def plotMeans(ax, mus, centroids, k):
+    mus.plot(ax = ax, kind="scatter", x=0, y=1, figsize=f_size, color="red")
     for i in range(k):
-        x = mu0[i][0]
-        y = mu0[i][1]
+        x = centroids[i][0]
+        y = centroids[i][1]
         ax.annotate(u'µ_'+str(i+1), xy=(x,y), xytext=(x+0.2,y+0.2))
-        
-    
-kmeans(dataset, 5)
+
+kmeans(dataset, k=5)
