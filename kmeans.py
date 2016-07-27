ae88257a23e3e7454de46aaeb412011fba99489b
emankcin <joknaut@gmx.net>
2016-07-27 21:36:40 +0200

    Refactor kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -20,39 +20,41 @@ maxY = max(dataset[1])
 xDist = np.absolute(maxX - minX)
 yDist = np.absolute(maxY - minY)
 
+def generatePoint():
+    return [minX + np.random.rand() * xDist, minY + np.random.rand() * yDist]
 
 def kmeans(data, k=4):
     centroids = []  
     for i in range(k):
-        centroids.append([minX + np.random.rand() * xDist, minY + np.random.rand() * yDist])
-    mus = pd.DataFrame(centroids)
+        centroids.append(generatePoint())
+    centroidsFrame = pd.DataFrame(centroids)
     clusters = []
     for i in range(k):
         clusters.append([])
-    kmeans_h(data, k, centroids, clusters, mus)
+    kmeans_h(data, k, centroids, clusters, centroidsFrame)
 
-def kmeans_h(data, k, centroids, clusters, mus):
+def kmeans_h(data, k, centroids, clusters, centroidsFrame):
     ax = data.plot(kind="scatter", x=0, y=1, figsize=f_size)
-    plotMeans(ax, mus, centroids, k)
+    plotMeans(ax, centroidsFrame, centroids, k)
     plt.show()
     for i in range(k):
         clusters[i] = []
     for i in range(len(dataset)):
         idx = getNearestMeanIndex([data[0][i], data[1][i]], centroids)
         clusters[idx].append([data[0][i], data[1][i]])
-    (hasChanged, centroids, mus) = updateMeans(clusters, centroids)
+    (hasChanged, centroids, centroidsFrame) = updateMeans(clusters, centroids)
     if True == hasChanged:
-        kmeans_h(data, k, centroids, clusters, mus)        
+        kmeans_h(data, k, centroids, clusters, centroidsFrame)        
         for i in range(k):
             for j in range(k):
                 if (centroids[i][0] - centroids[j][0])**2 + (centroids[i][1] - centroids[j][1])**2 < 5:
-                    centroids[j] = [minX + np.random.rand() * xDist, minY + np.random.rand() * yDist]
+                    centroids[j] = generatePoint()
     
 def updateMeans(clusters, centroids):
     flag = False
     for i in range(len(clusters)):
         if clusters[i] == []:
-            centroids[i] = [minX + np.random.rand() * xDist, minY + np.random.rand() * yDist] 
+            centroids[i] = generatePoint() 
             continue       
         if flag == False:
             if np.absolute(centroids[i][0] - np.mean(clusters[i], axis=0)[0]) > 0.01:
@@ -72,8 +74,8 @@ def getNearestMeanIndex(point, means):
             minidx = i
     return minidx
 
-def plotMeans(ax, mus, centroids, k):
-    mus.plot(ax = ax, kind="scatter", x=0, y=1, figsize=f_size, color="red")
+def plotMeans(ax, centroidsFrame, centroids, k):
+    centroidsFrame.plot(ax = ax, kind="scatter", x=0, y=1, figsize=f_size, color="red")
     for i in range(k):
         x = centroids[i][0]
         y = centroids[i][1]
