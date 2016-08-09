eb84991acca524ad38afbe722d270df659439c76
emankcin <joknaut@gmx.net>
2016-08-09 11:25:56 +0200

    Implement insert and contains functions of frequent pattern tree

--- src/main/python/kdd/fitemset/fptree/fptree.py
+++ src/main/python/kdd/fitemset/fptree/fptree.py
@@ -1,26 +1,27 @@
 class FrequentPatternTree():
-    def __init__(self):
-        self.value = -1
-        self.prefix = []
+    def __init__(self, value):
+        self.value = value
         self.count = 0
         self.children = {}
-        self.level = 0
 
     def insert(self, item_list):
-        # TODO: work in progress
-
         if item_list == []:
             pass
         else:
             first_item = item_list[0]
-            if self.level == 0:
-                if first_item in self.children:
-                    self.children[first_item].insert(item_list[1:])
-            if self.value == first_item:
-                self.count += 1
+            if not first_item in self.children:
+                self.children[first_item] = FrequentPatternTree(first_item)
+            self.count += 1
+            self.children[first_item].insert(item_list[1:])
 
     def contains(self, item):
-        return False
+        if self.value == item:
+            return True
+        else:
+            for key in self.children:
+                if True == self.children[key].contains(item):
+                    return True
+            return False
 
 
 def _get_desc_list_of_frequent_one_items(self, data_set, min_sup):
@@ -30,4 +31,7 @@ def _rearrange_data_set_according_to_one_items(self, data_set, desc_list):
     return []
 
 def _generate_frequent_pattern_tree(self, data_set):
-    return FrequentPatternTree()
+    fpt = FrequentPatternTree(-1)
+    for item_list in data_set:
+        fpt.insert(item_list)
+    return fpt
