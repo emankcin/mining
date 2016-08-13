9edac7f3071cadd0dd33e7dabb50393351ae7604
emankcin <joknaut@gmx.net>
2016-08-13 11:11:59 +0200

    Refactor apriori function

--- src/main/python/kdd/fitemset/apriori/apriori.py
+++ src/main/python/kdd/fitemset/apriori/apriori.py
@@ -6,12 +6,14 @@ from join import join
 
 def _retrieve_frequent_singleton_counts(item_lists, min_sup):
     """
-    Counts each item in a list of item lists. Non-frequent items are discarded.
+    Counts each item in a list of item lists. Only frequent items are returned.
+
+    An item set is frequent if its count is greater than or equal to the minimum support.
 
     :param item_lists: list of integer lists
     :param min_sup: minimum support
     :return: dictionary with frequent singletons and their counts
-    :rtype: Dict[Tuple[int], int]
+    :rtype: Dict[Tuple[int] : int]
 
     Example:
 
@@ -28,7 +30,9 @@ def _retrieve_frequent_singleton_counts(item_lists, min_sup):
     # scan data_set to count singleton item occurrences
     for item_list in item_lists:
         for item in item_list:
+
             key = (item,)
+
             if not key in counts:
                 counts[key] = 1
             else:
@@ -55,31 +59,27 @@ def _item_lists_self_join(item_lists):
     """
 
     join_result = []
-
     length = len(item_lists)
 
     for i in range(length):
-
         for j in range(i + 1, length):
-
             joined = join(item_lists[i], item_lists[j])
-
             if joined:
                 join_result.append(joined)
 
     return join_result
 
 
-def _get_frequent_n_itemsets(data_set, n_item_lists, min_sup):
+def _get_frequent_n_item_tuples_with_counts(data_set, n_item_lists, min_sup):
     """
     Example:
 
-    >>> from kdd.fitemset.apriori.apriori import _get_frequent_n_itemsets
+    >>> from kdd.fitemset.apriori.apriori import _get_frequent_n_item_tuples_with_counts
 
     >>> data_set = [[1,2,3,4,5], [2,3,4,5,6], [3,4,5,6,7]]
     >>> n_item_lists = [[2,3], [3,4]]
     >>> min_sup = 2
-    >>> _get_frequent_n_itemsets(data_set, n_item_lists, min_sup)
+    >>> _get_frequent_n_item_tuples_with_counts(data_set, n_item_lists, min_sup)
     {(3, 4): 3, (2, 3): 2}
     """
 
@@ -87,7 +87,6 @@ def _get_frequent_n_itemsets(data_set, n_item_lists, min_sup):
 
     # count how often each of the k n_item_lists is subset of a data row
     for row in data_set:
-
         for n_item_list in n_item_lists:
 
             contained = set(n_item_list).issubset(set(row))
@@ -97,11 +96,8 @@ def _get_frequent_n_itemsets(data_set, n_item_lists, min_sup):
                 n_item_tuple = tuple(n_item_list)
 
                 if not n_item_tuple in k_counts:
-
                     k_counts[n_item_tuple] = 1
-
                 else:
-
                     k_counts[n_item_tuple] += 1
 
     result = {}
@@ -117,19 +113,32 @@ def _get_frequent_n_itemsets(data_set, n_item_lists, min_sup):
 
     return result
 
+def _get_frequent_n_item_lists(data_set, n_item_lists, min_sup):
+    """
+        Example:
 
-def _construct_hash_tree(dic):
-    if len(dic.values()) > 0:
-        k = len(dic.values()[0])
+        >>> from kdd.fitemset.apriori.apriori import _get_frequent_n_item_lists
+
+        >>> data_set = [[1,2,3,4,5], [2,3,4,5,6], [3,4,5,6,7]]
+        >>> n_item_lists = [[2,3], [3,4]]
+        >>> min_sup = 2
+        >>> _get_frequent_n_item_lists(data_set, n_item_lists, min_sup)
+        [[3, 4], [2, 3]]
+        """
+    return [list(tup) for tup in _get_frequent_n_item_tuples_with_counts(data_set, n_item_lists, min_sup).keys()]
+
+def _construct_hash_tree(item_lists):
+    if len(item_lists) > 0:
+        k = len(item_lists[0])
     else:
         k = 0
     ht = HashTree(k, 0)
-    for i in dic.values():
-        ht.insert(tuple(i))
+    for item_list in item_lists:
+        ht.insert(tuple(item_list))
     return ht
 
 
-def _get_f_n_itemsets_w_hashtree(dataset, hash_tree, min_sup):
+def _get_frequent_n_item_tuples_with_counts_by_hash_tree(dataset, hash_tree, min_sup):
     result_dic = {}
     for ta in dataset:
         ta = [int(float(i)) for i in ta]
@@ -145,38 +154,51 @@ def _get_f_n_itemsets_w_hashtree(dataset, hash_tree, min_sup):
     return result_dic
 
 
-def apriori(dataset, max_items, min_sup, with_hash_tree=True):
+def apriori(data_set, min_sup, with_hash_tree=True):
     """
-    The apriori algorithm for retrieving frequent item sets.
+    The apriori algorithm for retrieving frequent item tuples together with their counts.
 
-    :param dataset: A list of integer-lists (integers usually represent the keys of the items)
-    :param max_items: maximum number of different items
+    :param data_set: A list of integer-lists (integers usually represent the keys of the items)
     :param min_sup: minimum support, i.e. minimum number of items for them to be considered frequent
     :param with_hash_tree: True is default and means that the hash tree variant is used. False uses simple variant.
     :type with_hash_tree: bool.
+    :return: dictionary of frequent item tuples with their counts
+    :rtype: Dict[Tuple[int] : int]
+
+    Example:
+
+    >>> from kdd.fitemset.apriori.apriori import apriori
+
+    >>> data_set = [[6], [7], [8], [3,6], [4,7], [5,8], [1,2,3], [7,8,9], [6,7,8,9]]
+    >>> min_sup = 3
+    >>> apriori(data_set, min_sup)
+    {(8,): 4, (6,): 3, (7,): 4}
+    >>> min_sup = 2
+    >>> apriori(data_set, min_sup)
+    {(8,): 4, (3,): 2, (7, 8, 9): 2, (9,): 2, (8, 9): 2, (6,): 3, (7,): 4, (7, 8): 2, (7, 9): 2}
     """
-    result = {}
-
-    k_result = _retrieve_frequent_singleton_counts(dataset, min_sup)
-
-    result.update(k_result)
+    result =  _retrieve_frequent_singleton_counts(data_set, min_sup)
+    k_result = result.copy()
 
     while True:
-        k_result = [list(tup) for tup in k_result.keys()]
-        k_result = _item_lists_self_join(k_result)
-        #k_result = dict(zip([i for i in range(len(k_result))], k_result))
 
-        if k_result == []:
-            break
-        if (with_hash_tree):
-            hash_tree = _construct_hash_tree(k_result)
-            k_result = _get_f_n_itemsets_w_hashtree(dataset, hash_tree, min_sup)
-        else:
-            k_result = _get_frequent_n_itemsets(dataset, k_result, min_sup)
-        if k_result == {}:
+        last_k_result = [list(tup) for tup in k_result.keys()]
+        new_k_input = _item_lists_self_join(last_k_result)
+
+        if new_k_input == []:
             break
         else:
-            result.update(k_result)
+
+            if (with_hash_tree):
+                hash_tree = _construct_hash_tree(new_k_input)
+                k_result = _get_frequent_n_item_tuples_with_counts_by_hash_tree(data_set, hash_tree, min_sup)
+            else:
+                k_result = _get_frequent_n_item_tuples_with_counts(data_set, new_k_input, min_sup)
+
+            if k_result == {}:
+                break
+            else:
+                result.update(k_result)
 
     #print "result: "
     #print result
--- src/unittest/python/apriori_tests.py
+++ src/unittest/python/apriori_tests.py
@@ -1,11 +1,25 @@
 from kdd.fitemset.apriori.apriori import _retrieve_frequent_singleton_counts, apriori, \
-    _get_frequent_n_itemsets, _item_lists_self_join, _construct_hash_tree, _get_f_n_itemsets_w_hashtree
+    _get_frequent_n_item_tuples_with_counts, _item_lists_self_join, _construct_hash_tree, _get_frequent_n_item_tuples_with_counts_by_hash_tree, _get_frequent_n_item_lists
 from fitemset_testbase import FItemsetTestBase
 
 
 class AprioriTest(FItemsetTestBase):
 
-    def test_get_frequent_one_itemsets(self):
+    def test_apriori_with_hash_tree(self):
+        self.assertEqual(self.f_three_itemsets, apriori(self.dataset, 3, with_hash_tree=True))
+        self.assertEqual(self.f_four_itemsets, apriori(self.dataset, 4, with_hash_tree=True))
+        self.assertEqual(self.f_five_itemsets, apriori(self.dataset, 5, with_hash_tree=True))
+        self.assertEqual(self.f_six_itemsets, apriori(self.dataset, 6, with_hash_tree=True))
+        self.assertEqual(self.f_seven_itemsets, apriori(self.dataset, 7, with_hash_tree=True))
+
+    def test_apriori_without_hash_tree(self):
+        self.assertEqual(self.f_three_itemsets, apriori(self.dataset, 3, with_hash_tree=False))
+        self.assertEqual(self.f_four_itemsets, apriori(self.dataset, 4, with_hash_tree=False))
+        self.assertEqual(self.f_five_itemsets, apriori(self.dataset, 5, with_hash_tree=False))
+        self.assertEqual(self.f_six_itemsets, apriori(self.dataset, 6, with_hash_tree=False))
+        self.assertEqual(self.f_seven_itemsets, apriori(self.dataset, 7, with_hash_tree=False))
+
+    def test_retrieve_singleton_counts(self):
         self.assertEqual({(2,): 6, (0,): 4, (3,): 7, (1,): 6}, _retrieve_frequent_singleton_counts(self.dataset, 3))
         self.assertEqual({(2,): 6, (0,): 4, (3,): 7, (1,): 6}, _retrieve_frequent_singleton_counts(self.dataset, 4))
         self.assertEqual({(2,): 6, (3,): 7, (1,): 6}, _retrieve_frequent_singleton_counts(self.dataset, 5))
@@ -13,36 +27,28 @@ class AprioriTest(FItemsetTestBase):
         self.assertEqual({(3,): 7}, _retrieve_frequent_singleton_counts(self.dataset, 7))
         self.assertEqual({}, _retrieve_frequent_singleton_counts(self.dataset, 8))
 
-    def test_apriori_with_hash_tree(self):
-        self.assertEqual(self.f_three_itemsets, apriori(self.dataset, self.n, 3, with_hash_tree=True))
-        self.assertEqual(self.f_four_itemsets, apriori(self.dataset, self.n, 4, with_hash_tree=True))
-        self.assertEqual(self.f_five_itemsets, apriori(self.dataset, self.n, 5, with_hash_tree=True))
-        self.assertEqual(self.f_six_itemsets, apriori(self.dataset, self.n, 6, with_hash_tree=True))
-        self.assertEqual(self.f_seven_itemsets, apriori(self.dataset, self.n, 7, with_hash_tree=True))
-
-    def test_apriori_without_hash_tree(self):
-        self.assertEqual(self.f_three_itemsets, apriori(self.dataset, self.n, 3, with_hash_tree=False))
-        self.assertEqual(self.f_four_itemsets, apriori(self.dataset, self.n, 4, with_hash_tree=False))
-        self.assertEqual(self.f_five_itemsets, apriori(self.dataset, self.n, 5, with_hash_tree=False))
-        self.assertEqual(self.f_six_itemsets, apriori(self.dataset, self.n, 6, with_hash_tree=False))
-        self.assertEqual(self.f_seven_itemsets, apriori(self.dataset, self.n, 7, with_hash_tree=False))
-
     def test_item_lists_self_join(self):
         self.assertEqual([[1, 2, 3, 4], [1, 2, 3, 5], [1, 2, 4, 5]],
                          _item_lists_self_join([[1, 2, 3], [6, 7, 8], [1, 2, 4], [1, 2, 5]]))
 
-    def test_get_frequent_n_itemsets(self):
+    def test_get_frequent_n_item_tuples_with_counts(self):
         current_list = [[1, 2], [1, 3], [2, 3]]
         min_sup = 4
-        self.assertEqual([(1, 2), (1, 3), (2, 3)],
-                         _get_frequent_n_itemsets(self.dataset, current_list, min_sup).keys())
+        self.assertEqual({(1, 2): 4, (1, 3): 4, (2, 3): 4},
+                         _get_frequent_n_item_tuples_with_counts(self.dataset, current_list, min_sup))
+
+    def test_get_frequent_n_item_lists(self):
+        current_list = [[1, 2], [1, 3], [2, 3]]
+        min_sup = 4
+        self.assertEqual([[1, 2], [1, 3], [2, 3]],
+                         _get_frequent_n_item_lists(self.dataset, current_list, min_sup))
 
     def test_construct_hash_tree(self):
-        ht = _construct_hash_tree({0: (1, 2), 1: (2, 3)})
+        ht = _construct_hash_tree([[1, 2], [2, 3]])
         self.assertTrue(ht.contains((1, 2)))
         self.assertFalse(ht.contains((1, 3)))
 
-    def test_get_f_n_itemsets_w_hashtree(self):
-        ht = _construct_hash_tree({0: (1, 2), 1: (1, 3), 2: (2, 3)})
+    def test_get_frequent_n_item_tuples_with_counts_by_hash_tree(self):
+        ht = _construct_hash_tree([[1, 2], [1, 3], [2, 3]])
         min_sup = 4
-        self.assertEqual({(1, 2): 4, (2, 3): 4, (1, 3): 4}, _get_f_n_itemsets_w_hashtree(self.dataset, ht, min_sup))
+        self.assertEqual({(1, 2): 4, (2, 3): 4, (1, 3): 4}, _get_frequent_n_item_tuples_with_counts_by_hash_tree(self.dataset, ht, min_sup))
