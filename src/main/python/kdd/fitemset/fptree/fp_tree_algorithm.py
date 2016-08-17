327267a211eebab9711b2aeec0a47e0255cb3acd
emankcin <joknaut@gmx.net>
2016-08-18 00:17:22 +0200

    Fix doctests of fp tree algorithm

--- src/main/python/kdd/fitemset/fptree/fp_tree_algorithm.py
+++ src/main/python/kdd/fitemset/fptree/fp_tree_algorithm.py
@@ -91,7 +91,9 @@ def retrieve_frequent_item_tuples(data_set, min_sup):
     :rtype: List[Tuple[int]]
 
     Example:
+
     >>> from kdd.fitemset.fptree.fp_tree_algorithm import retrieve_frequent_item_tuples
+
     >>> data_set = [[1,2], [1,3], [1,4], [2], [2,3,4], (3,4)]
     >>> min_sup = 2
     >>> retrieve_frequent_item_tuples(data_set, min_sup)
